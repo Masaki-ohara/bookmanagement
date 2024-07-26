39c70fb0a6cabe0ce3534ea9ed478895f6177e81
Masaki-Ohara <masakingu921@gmail.com>
2024-07-26 21:24:52 +0900

    本の削除機能を作成

--- src/main/java/com/example/bookmanagement/model/Book.java
+++ src/main/java/com/example/bookmanagement/model/Book.java
@@ -1,15 +1,19 @@
 package com.example.bookmanagement.model;
 
 import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.hibernate.annotations.SQLRestriction;
+// ↑↑↑↑
+// 同じSQLRestrictionでもimport元が違うものがあるので注意
+// import org.hibernate.annotations.DialectOverride.SQLRestriction;
 
 @Entity
+@SQLRestriction("is_deleted = false")
 @Table(name = "books")
 public class Book {
     @Id
@@ -24,6 +28,9 @@ public class Book {
 
     @Column(nullable = false, unique = true)
     private String isbn;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
 
     private LocalDate publishedDate;
 
@@ -78,4 +85,13 @@ public class Book {
     public void setPublishedDate(LocalDate publishedDate) {
         this.publishedDate = publishedDate;
     }
+
+    public Boolean getIsDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean isDeleted) {
+        this.isDeleted = isDeleted;
+    }
 }
+
